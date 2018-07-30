5a819ca40fda7752ee8843ecdfe41e45a643b0f3
lhlyh <1939029018@qq.com>
2018-07-30 21:51:15 +0800

    project init

--- GitInit.bat
+++ GitInit.bat
@@ -3,12 +3,18 @@
 git init
 git status
 
-set /p fileAdd= 请输入要添加的文件
-git add %fileAdd%
+set /p AddFile= 请输入git add的文件:
+git add %AddFile%
 git commit -m "project init"
 
-set /p gitURL= 请输入GitHub地址: 
-git remote add origin %gitURL%
+set /p n= 是否上传到GitHub(y/n)?
+if %n%==y ( goto GITHUB )
+else if %n%==n ( goto EXIT )
+
+:GITHUB
+set /p GitURL= 请输入GitHub地址: 
+git remote add origin %GitURL%
 git push -u origin master
 
+:EXIT
 pause
