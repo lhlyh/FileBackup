4f50b54de7937c8d9f33af81c8d08f239d8db72a
lhlyh <1939029018@qq.com>
2018-07-30 21:58:07 +0800

    修改GitInit 增加是否需要上传到github

--- GitInit.bat
+++ GitInit.bat
@@ -8,8 +8,8 @@ git add %AddFile%
 git commit -m "project init"
 
 set /p n= 是否上传到GitHub(y/n)?
-if %n%==y ( goto GITHUB )
-else if %n%==n ( goto EXIT )
+if %n%==y ( goto GITHUB 
+) else ( if %n%==n ( goto EXIT ) )
 
 :GITHUB
 set /p GitURL= 请输入GitHub地址: 
@@ -17,4 +17,5 @@ git remote add origin %GitURL%
 git push -u origin master
 
 :EXIT
+echo Git init done!
 pause
